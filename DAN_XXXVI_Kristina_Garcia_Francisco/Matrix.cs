f7262685b797f5f5adf2237cedd8dd39dcab7543
kris994 <kristipomo@gmail.com>
2020-06-22 15:00:56 +0200

    Added writing and reading from file

--- DAN_XXXVI_Kristina_Garcia_Francisco/Matrix.cs
+++ DAN_XXXVI_Kristina_Garcia_Francisco/Matrix.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace DAN_XXXVI_Kristina_Garcia_Francisco
 {
@@ -13,12 +11,17 @@ namespace DAN_XXXVI_Kristina_Garcia_Francisco
         private int[,] matrixArray;
         private readonly object lockMatrix = new object();
         private Random rng = new Random();
+        private readonly string matrixFile = "matrix.txt";
+        private bool fileCreated = false;
 
         public void CreateMatrix()
         {
             lock (lockMatrix)
             {
                 matrixArray = new int[100, 100];
+
+                // Inform the thread that the object is available.
+                Monitor.Pulse(lockMatrix);
                 Monitor.Wait(lockMatrix);
 
                 int matrixLength = matrixValues.Length;
@@ -27,7 +30,7 @@ namespace DAN_XXXVI_Kristina_Garcia_Francisco
                 {
                     for (int j = 0; j < matrixArray.GetLength(1); j++)
                     {
-                        matrixArray[i, j] = matrixValues[(i+1)*j];
+                        matrixArray[i, j] = matrixValues[(i + 1) * j];
                     }
                 }
             }     
@@ -35,11 +38,14 @@ namespace DAN_XXXVI_Kristina_Garcia_Francisco
 
         public void GenerateMatrixValues()
         {
-            // Making sure the getValues thread only starts after the 
-            // first part of createM thread is done
-            Thread.Sleep(10);
             lock (lockMatrix)
             {
+                // If the matrix is empty lock this thread.
+                if (matrixArray == null)
+                {
+                    Monitor.Wait(lockMatrix);
+                }
+
                 int matrixLength = matrixValues.Length;
                 int value = rng.Next(10, 100);
                 for (int i = 0; i < matrixLength; i++)
@@ -49,7 +55,66 @@ namespace DAN_XXXVI_Kristina_Garcia_Francisco
                 }
 
                 Monitor.Pulse(lockMatrix);
-                Monitor.Wait(lockMatrix);
+            }
+        }
+
+        public void OddNumberToFile()
+        {
+            lock (lockMatrix)
+            {
+                int countOdd = 0;
+                List<int> allOddNumbers = new List<int>();
+
+                foreach (var item in matrixArray)
+                {
+                    if (item % 2 == 1)
+                    {
+                        countOdd++;
+                        allOddNumbers.Add(item);
+                    }
+                }
+
+                int totalOddNumbers = allOddNumbers.Count;
+                int[] matrixOddValues = new int[totalOddNumbers];
+
+                for (int i = 0; i < totalOddNumbers; i++)
+                {
+                    matrixOddValues[i] = allOddNumbers[i];
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter(matrixFile))
+                {
+                    for (int i = 0; i < totalOddNumbers; i++)
+                    {
+                        streamWriter.WriteLine(matrixOddValues[i] + " ");
+                    }
+                }
+
+                fileCreated = true;
+
+                // If the thread tried to run first but failed due to the lock, now it can run again
+                Monitor.Pulse(lockMatrix);
+            }
+        }
+
+        public void ReadFromFile()
+        {
+            lock (lockMatrix)
+            {
+                // Dont run this thread until the file is created
+                if (fileCreated == false)
+                {
+                    Monitor.Wait(lockMatrix);
+                }
+
+                using (StreamReader streamReader = File.OpenText(matrixFile))
+                {
+                    string line;
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        Console.Write(line);
+                    }
+                }
             }
         }
 
@@ -63,6 +128,12 @@ namespace DAN_XXXVI_Kristina_Garcia_Francisco
 
             createM.Join();
             getValues.Join();
+
+            Thread oddNumber = new Thread(OddNumberToFile);
+            Thread readFile = new Thread(ReadFromFile);
+
+            oddNumber.Start();
+            readFile.Start();
         }
     }
 }
